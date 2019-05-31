19b0df1ffb0b9fb243c82e7083c38c9855dfacef
m-zubairahmed <m-zubairahmed@protonmail.com>
2019-06-01 03:52:11 +0530

    added lots of other routes

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@
 module github.com/m-zubairahmed/sardene-api
 
 require (
-	github.com/gin-contrib/cors v1.3.0 // indirect
+	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/go-stack/stack v1.8.0 // indirect
--- main.go
+++ main.go
@@ -22,6 +22,7 @@ type IdeaStructure struct {
 	ID          primitive.ObjectID `json:"id" bson:"_id"`
 	Name        string             `json:"name" bson:"name"`
 	Description string             `json:"description" bson:"description"`
+	Publisher   string             `json:"publisher" bson:"publisher"`
 	Makers      int64              `json:"makers" bson:"makers"`
 	Gazers      int64              `json:"gazers" bson:"gazers"`
 	CreatedAt   int64              `json:"created_at" bson:"created_at"`
@@ -43,7 +44,7 @@ func connectToDatabase() *mongo.Client {
 	databaseClient, errInConnection := mongo.Connect(connectContext, connectOptions)
 
 	if errInConnection != nil {
-		log.Fatal(errInConnection , "Failed to connect to DB")
+		log.Fatal(errInConnection, "Failed to connect to DB")
 	}
 
 	errInPing := databaseClient.Ping(connectContext, nil)
@@ -56,15 +57,9 @@ func connectToDatabase() *mongo.Client {
 }
 
 func welcome(gContext *gin.Context) {
-	message := "Welcome to Sardene API, \nVisit https://github.com/M-ZubairAhmed/Sardene-API for documentation."
+	message := "Welcome to Sardene API, \nServer running successfully" + string(http.StatusOK) +
+		"\nVisit https://github.com/M-ZubairAhmed/Sardene-API for documentation."
 	gContext.String(http.StatusOK, message)
-}
-
-func ping(gContext *gin.Context) {
-	gContext.JSON(http.StatusOK, gin.H{
-		"status":  http.StatusOK,
-		"message": "pinged success",
-	})
 }
 
 func getIdeas(gContext *gin.Context, databaseClient *mongo.Client) {
@@ -81,7 +76,8 @@ func getIdeas(gContext *gin.Context, databaseClient *mongo.Client) {
 	if errorInFinding != nil {
 		_ = ideasCursor.Close(databaseContext)
 		databaseContext.Done()
-		gContext.JSON(http.StatusServiceUnavailable, gin.H{"status": http.StatusServiceUnavailable, "error": "Error in searching database"})
+		gContext.JSON(http.StatusServiceUnavailable, gin.H{"status": http.StatusServiceUnavailable,
+			"error": "Error in searching database"})
 		return
 	}
 
@@ -92,7 +88,8 @@ func getIdeas(gContext *gin.Context, databaseClient *mongo.Client) {
 		if errInDecoding != nil {
 			_ = ideasCursor.Close(databaseContext)
 			databaseContext.Done()
-			gContext.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError, "error": "Error in decoding database"})
+			gContext.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError,
+				"error": "Error in decoding database"})
 			log.Fatal(errInDecoding)
 		}
 
@@ -103,20 +100,22 @@ func getIdeas(gContext *gin.Context, databaseClient *mongo.Client) {
 	if errInCursor != nil {
 		databaseContext.Done()
 		_ = ideasCursor.Close(databaseContext)
-		gContext.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError, "error": "Error while iterating database"})
+		gContext.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError,
+			"error": "Error while iterating database"})
 	}
 
 	errInClosingCursor := ideasCursor.Close(databaseContext)
-	if errInClosingCursor != nil{
+	if errInClosingCursor != nil {
 		databaseContext.Done()
-		gContext.JSON(http.StatusServiceUnavailable, gin.H{"status": http.StatusServiceUnavailable, "error": "Error while closing iterator of database"})
+		gContext.JSON(http.StatusServiceUnavailable, gin.H{"status": http.StatusServiceUnavailable,
+			"error": "Error while closing iterator of database"})
 		return
 	}
 
 	lengthOfIdeas := len(ideas)
+
 	gContext.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": ideas, "count": lengthOfIdeas})
 	databaseContext.Done()
-
 	return
 }
 
@@ -154,11 +153,13 @@ func addIdea(gContext *gin.Context, databaseClient *mongo.Client) {
 	// Defaulting data
 	jsonInput.Makers = 0
 	jsonInput.Gazers = 0
+	jsonInput.Publisher = "Unnamed contact"
 	jsonInput.CreatedAt = createdTime
 
 	ideaToAdd := bson.M{
 		"name":        jsonInput.Name,
 		"description": jsonInput.Description,
+		"publisher":   jsonInput.Publisher,
 		"makers":      jsonInput.Makers,
 		"gazers":      jsonInput.Gazers,
 		"created_at":  createdTime,
@@ -167,7 +168,7 @@ func addIdea(gContext *gin.Context, databaseClient *mongo.Client) {
 	addedIdea, errInAdding := ideasCollection.InsertOne(databaseContext, ideaToAdd)
 	if errInAdding != nil {
 		gContext.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError,
-			"error":     "Error while saving to database"})
+			"error": "Error while saving to database"})
 		return
 	}
 
@@ -177,6 +178,163 @@ func addIdea(gContext *gin.Context, databaseClient *mongo.Client) {
 	gContext.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "data": jsonInput})
 	databaseContext.Done()
 	return
+}
+
+func gazeIdea(ginContext *gin.Context, databaseClient *mongo.Client, ideaID string) {
+	ideasCollection := databaseClient.Database("sardene-db").Collection("ideas")
+
+	databaseContext, cancelContext := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancelContext()
+
+	hexIdeaID, errInValidatingID := primitive.ObjectIDFromHex(ideaID)
+	if errInValidatingID != nil {
+		databaseContext.Done()
+		ginContext.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest,
+			"error": "Error, Idea id is not valid"})
+		return
+	}
+
+	findIdeaFilter := bson.M{"_id": hexIdeaID}
+	updateGazeOfIdea := bson.M{"$inc": bson.M{"gazers": 1}}
+
+	updatedIdea, errInFindingIdea := ideasCollection.UpdateOne(databaseContext, findIdeaFilter, updateGazeOfIdea)
+	if errInFindingIdea != nil {
+		databaseContext.Done()
+		ginContext.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "error": "Error, Idea not found"})
+		return
+	}
+
+	ginContext.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": "",
+		"message": "Increased gaze count of " + string(updatedIdea.ModifiedCount) + "idea"})
+	databaseContext.Done()
+	return
+}
+
+func makeIdea(ginContext *gin.Context, databaseClient *mongo.Client, ideaID string) {
+	ideasCollection := databaseClient.Database("sardene-db").Collection("ideas")
+
+	databaseContext, cancelContext := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancelContext()
+
+	hexIdeaID, errInValidatingID := primitive.ObjectIDFromHex(ideaID)
+	if errInValidatingID != nil {
+		databaseContext.Done()
+		ginContext.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest,
+			"error": "Error, Idea id is not valid"})
+		return
+	}
+
+	findIdeaFilter := bson.M{"_id": hexIdeaID}
+	updateGazeOfIdea := bson.M{"$inc": bson.M{"makers": 1}}
+
+	updatedIdea, errInFindingIdea := ideasCollection.UpdateOne(databaseContext, findIdeaFilter, updateGazeOfIdea)
+	if errInFindingIdea != nil {
+		databaseContext.Done()
+		ginContext.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "error": "Error, Idea not found"})
+		return
+	}
+
+	ginContext.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": "",
+		"message": "Increased make count of " + string(updatedIdea.ModifiedCount) + "idea"})
+	databaseContext.Done()
+	return
+
+}
+
+func updateIdea(ginContext *gin.Context, databaseClient *mongo.Client, ideaID string) {
+	ideasCollection := databaseClient.Database("sardene-db").Collection("ideas")
+
+	databaseContext, cancelContext := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancelContext()
+
+	hexIdeaID, errInValidatingID := primitive.ObjectIDFromHex(ideaID)
+	if errInValidatingID != nil {
+		databaseContext.Done()
+		ginContext.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest,
+			"error": "Error, Idea id is not valid"})
+		return
+	}
+
+	var jsonInput IdeaStructure
+
+	errInInputJSON := ginContext.ShouldBindJSON(&jsonInput)
+	if errInInputJSON != nil {
+		ginContext.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest,
+			"error": "Wrong structure of posted data", "errorDetails": errInInputJSON})
+		databaseContext.Done()
+		return
+	}
+
+	lengthOfName := len(strings.TrimSpace(jsonInput.Name))
+	lengthOfDescription := len(strings.TrimSpace(jsonInput.Description))
+
+	if lengthOfName == 0 && lengthOfDescription == 0 {
+		ginContext.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest,
+			"error": "Both name and description are empty"})
+		databaseContext.Done()
+		return
+	}
+
+	filterOfUpdatingIdea := bson.M{"_id": hexIdeaID}
+	var updateIdea bson.M
+
+	if lengthOfName == 0 && lengthOfDescription != 0 {
+		// Updating only description
+		updateIdea = bson.M{"$set": bson.M{
+			"description": jsonInput.Description,
+		}}
+	} else if lengthOfName != 0 && lengthOfDescription == 0 {
+		// Updating only name
+		updateIdea = bson.M{"$set": bson.M{
+			"name": jsonInput.Name,
+		}}
+	} else {
+		// updating both
+		updateIdea = bson.M{"$set": bson.M{
+			"name":        jsonInput.Name,
+			"description": jsonInput.Description,
+		}}
+	}
+
+	_, errInFindingIdea := ideasCollection.UpdateOne(databaseContext, filterOfUpdatingIdea, updateIdea)
+	if errInFindingIdea != nil {
+		databaseContext.Done()
+		ginContext.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "error": "Error, Idea not found"})
+		return
+	}
+
+	ginContext.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Updated idea successfully"})
+	databaseContext.Done()
+	return
+}
+
+func deleteIdea(ginContext *gin.Context, databaseClient *mongo.Client, ideaID string) {
+	ideasCollection := databaseClient.Database("sardene-db").Collection("ideas")
+
+	databaseContext, cancelContext := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancelContext()
+
+	hexIdeaID, errInValidatingID := primitive.ObjectIDFromHex(ideaID)
+	if errInValidatingID != nil {
+		databaseContext.Done()
+		ginContext.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest,
+			"error": "Error, Idea id is not valid"})
+		return
+	}
+
+	findIdeaFilter := bson.M{"_id": hexIdeaID}
+
+	_, errInDeletingIdea := ideasCollection.DeleteOne(databaseContext, findIdeaFilter)
+	if errInDeletingIdea != nil {
+		databaseContext.Done()
+		ginContext.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "error": "Error, Idea not found"})
+		return
+	}
+
+	ginContext.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Idea deleted successfully"})
+	databaseContext.Done()
+	return
+
 }
 
 func main() {
@@ -189,26 +347,44 @@ func main() {
 
 	defaultCors := cors.DefaultConfig()
 
-	defaultCors.AllowOrigins = []string{ "http://localhost:3000"}
+	defaultCors.AllowOrigins = []string{"http://localhost:3000"}
 	router.Use(cors.New(defaultCors))
 
 	databaseClient := connectToDatabase()
 
 	router.GET("/", welcome)
 
-	router.GET("/ping", ping)
-
 	// TODO convert to pagination endpoint
-	// router.GET("/ideas/:page", getIdeas)
-	router.GET("/ideas", func (gContext *gin.Context){
+	router.GET("/ideas", func(gContext *gin.Context) {
 		getIdeas(gContext, databaseClient)
 	})
 
-	router.POST("/idea/add", func (gContext *gin.Context){
+	router.POST("/idea/add", func(gContext *gin.Context) {
 		addIdea(gContext, databaseClient)
 	})
 
-	// router.PUT("/updateidea/:ideaID", updateIdea)
+	router.PATCH("/idea/gaze/:ideaID", func(ginContext *gin.Context) {
+		ideaID := ginContext.Param("ideaID")
+		gazeIdea(ginContext, databaseClient, ideaID)
+	})
 
-	router.Run(":" + port)
+	router.PATCH("/idea/make/:ideaID", func(ginContext *gin.Context) {
+		ideaID := ginContext.Param("ideaID")
+		makeIdea(ginContext, databaseClient, ideaID)
+	})
+
+	router.PUT("/idea/update/:ideaID", func(ginContext *gin.Context) {
+		ideaID := ginContext.Param("ideaID")
+		updateIdea(ginContext, databaseClient, ideaID)
+	})
+
+	router.DELETE("/idea/delete/:ideaID", func(ginContext *gin.Context) {
+		ideaID := ginContext.Param("ideaID")
+		deleteIdea(ginContext, databaseClient, ideaID)
+	})
+
+	errInStartingServer := router.Run(":" + port)
+	if errInStartingServer != nil {
+		log.Fatal(errInStartingServer, "// Cannot start server")
+	}
 }
